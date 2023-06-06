171b6e537070d6efa0eefbb845c5ff9616b88e94
amelmnd <amelmnd.dev@gmail.com>
2023-06-06 15:58:13 +0200

    ft_printf : %c ok

--- ft_printf.c
+++ ft_printf.c
@@ -6,22 +6,96 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 12:09:32 by amennad           #+#    #+#             */
-/*   Updated: 2023/06/05 16:15:31 by amennad          ###   ########.fr       */
+/*   Updated: 2023/06/06 15:47:05 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+void	print_line(char check_char, va_list	args)
+{
+	if (check_char == 'c')
+		ft_putchar_fd(va_arg(args, int), 1);
+}
+
+
 int	ft_printf(const char	*format, ...)
 {
-	int	i;
+	va_list	args;
+	int		length;
+	int		i;
 
+	length = 0;
 	i = 0;
+	va_start(args, format);
 	while (format[i] != '\0')
 	{
-		printf("char : %c => %i\n", format[i], format[i]);
+		if (format[i] == '%')
+		{
+			print_line(format[i + 1], args);
+			i++;
+		}
+		else
+			ft_putchar_fd(format[i], 1);
 		i++;
 	}
-	printf("FORMAT : %s\n", format);
-	return (i);
+
+	return (length);
 }
+
+
+// void mdr_tibo_kastou(const char *str, ...)
+// {
+// 	int i;
+// 	va_list args;
+
+// 	va_start(args, format);
+// 	i = -1;
+// 	while(++i)
+// 		if (str[i] == '%')
+// 			handle_print(str[i + 1], args);
+// 		else
+// 			ft_putchar_fd(str[i], 1);
+// }
+// void handle_print(char c, va_list args)
+// {
+// 	if (c == 'c')
+// 		write(1, va_arg(args, int), 1);
+// // }
+//return position du 1 caracte ok dans le string
+// int	ft_str_index_str(const char *where, const char *what)
+// {
+// 	int	i;
+// 	int	y;
+
+// 	i = 0;
+// 	if (!what[0] || !where[0])
+// 		return (0);
+// 	while (where[i])
+// 	{
+// 		y = 0;
+// 		while (where[i + y] == what[y])
+// 		{
+// 			if (what[y + 1] == '\0')
+// 			{
+// 				return (i);
+// 			}
+// 			y++;
+// 		}
+// 		i++;
+// 	}
+// 	return (0);
+// }
+// int	ft_str_index_chr(char *str, char c)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+// 	while (i < ft_strlen(str) && str[i] != '\0')
+// 	{
+// 		if (str[i] == c)
+// 			return (i);
+// 		i++;
+// 	}
+// 	return (-1);
+// }
--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 12:09:44 by amennad           #+#    #+#             */
-/*   Updated: 2023/06/05 16:15:16 by amennad          ###   ########.fr       */
+/*   Updated: 2023/06/06 15:56:49 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // FONCTION TESTING FILE
+
 #include "ft_printf.h"
 #include <ctype.h>
 #include <stdio.h>
@@ -19,7 +20,60 @@
 
 int	main(void)
 {
-	// printf("empty flag\n");
-	ft_printf("empty flag\n+\n");
+
 	return (0);
 }
+
+//LES DIFFERENTS TESTS REALISES
+/*
+	printf("-----------------------------------------------------\n");
+	printf("SANS POURCENTAGE\n");
+	ft_printf("-----------------------------------------------------\n");
+	printf("----CAS STR----\n");
+	printf("TRUE : empty flag\n");
+	ft_printf("MY : empty flag\n");
+	printf("----CAS STR SPE CARA----\n");
+	printf("TRUE : empty flag &&\n");
+	ft_printf("MY : empty flag &&\n");
+	printf("----CAS STR ONLY SPE CARA----\n");
+	printf("@\n");
+	ft_printf("@\n");
+	printf("----CAS nbr----\n");
+	printf("125\n");
+	ft_printf("225\n");
+	printf("----CAS ONE CARA----\n");
+	printf("T\n");
+	ft_printf("m\n");
+	printf("----CAS CHAINE VIDE----\n");
+	printf("");
+	ft_printf("");
+	printf("----CAS VIDE----\n");
+	//GENERE MEME ERREUR DE COMPILATION
+	printf();
+	ft_printf();
+
+	printf("-----------------------------------------------------\n");
+	printf("POURCENTAGE C\n");
+	printf("-----------------------------------------------------\n");
+	printf("----NO AFTER----\n");
+	printf("----CAS CARA ----\n");
+	printf("TRUE : %c\n", 't');
+	ft_printf("MY : %c\n", 'm');
+	printf("----CAS SPE CARA----\n");
+	printf("TRUE : %c\n", '&');
+	ft_printf("MY : %c\n", '&');
+	printf("----CAS INT => CHAR----\n");
+	printf("TRUE : %c\n", 95);
+	ft_printf("MY : %c\n", 95);
+	printf("----WITH AFTER----\n");
+	printf("----CAS CARA ----\n");
+	printf("TRUE : %c true\n", 't');
+	ft_printf("MY : %c mine\n", 'm');
+	printf("----CAS SPE CARA----\n");
+	printf("TRUE : %c  true\n", '&');
+	ft_printf("MY : %c mine\n", '&');
+	printf("----CAS INT => CHAR----\n");
+	printf("TRUE : %c  true\n", 95);
+	ft_printf("MY : %c mine\n", 95);
+
+*/
