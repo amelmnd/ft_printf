01f13eee18ce12a0ab83cd54e686c7b631ec0aad
amelmnd <amelmnd.dev@gmail.com>
2023-06-08 10:16:26 +0200

    fr_printf : fix import lib method

--- Makefile
+++ Makefile
@@ -1,9 +1,7 @@
 # COLOR MAKEFILE
 COLOUR_GREEN=\033[0;32m
 COLOUR_RED=\033[0;31m
-COLOUR_BLUE=\033[0;36m
-COLOUR_YELLOW = \033[0;93m
-COLOUR_MAGENTA = \033[0;95m
+COLOUR_BLUE=\033[0;34m
 END_COLOR=\033[0m
 
 # VAR
@@ -20,30 +18,39 @@ OBJ = $(SRC:.c=.o)
 
 all: $(NAME)
 
-#generer le fichier principale de la lib (.a)
+libft:
+	@make -C libft_all
+	@echo "$(COLOUR_GREEN)libft compil 🟢$(END_COLOR)"
+
+#generer le fichier principale de l alib (.a)
 $(NAME):
 	$(CC) $(CFLAGS) -c $(SRC) -I libft
-	@echo "$(COLOUR_GREEN)printf compil 🍀$(END_COLOR)"
+	@echo "$(COLOUR_GREEN)printf compil 🟢$(END_COLOR)"
 	ar rc $(NAME) $(OBJ)
-	@echo "$(COLOUR_MAGENTA)libftprintf.a 👾$(END_COLOR)"
+	@echo "$(COLOUR_GREEN)libftprintf.a 🟢$(END_COLOR)"
 
 #compiler la lib (pour tester ou autre), necessite un main
 compil:
-	$(CC) $(CFLAGS) $(ORIGIN) $(SRC) -I libft -L libft -l ft -o $(EXEC)
+	$(CC) $(CFLAGS) $(ORIGIN) -I libft -L libft_all -l ft -o $(EXEC)
 	./$(EXEC)
 
 #supprimer les fichier .o
 clean:
+	cd ./libft_all && make clean
+	@echo "$(COLOUR_BLUE)libft clean 🔵$(END_COLOR)"
 	rm -rf *.o
-	@echo "$(COLOUR_BLUE)ft_printf clean 🐟$(END_COLOR)"
+	@echo "$(COLOUR_BLUE)ft_printf clean 🔵$(END_COLOR)"
 
 #supprimer les fichier .a, .o et l'executable
 fclean : clean
+	cd ./libft_all && make fclean
+	@echo "$(COLOUR_BLUE)libft fclean 🔵$(END_COLOR)"
 	rm -rf $(NAME) $(EXEC)
-	@echo "$(COLOUR_BLUE)ft_printf fclean 🐳$(END_COLOR)"
+	@echo "$(COLOUR_BLUE)ft_printf fclean 🔵$(END_COLOR)"
 
 re:fclean all
-	@echo "$(COLOUR_YELLOW)ft_printf re 🍋$(END_COLOR)"
+	@echo "$(COLOUR_BLUE)libft re 🔵$(END_COLOR)"
+	@echo "$(COLOUR_BLUE)ft_printf re 🔵$(END_COLOR)"
 
 norme:
 	norminette
--- ft_printf.c
+++ ft_printf.c
@@ -6,20 +6,27 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 12:09:32 by amennad           #+#    #+#             */
-/*   Updated: 2023/06/06 16:03:43 by amennad          ###   ########.fr       */
+/*   Updated: 2023/06/08 10:13:36 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	print_line(char check_char, va_list	args)
+void	print_line(char check_char, va_list	args, int *len)
 {
-	if (check_char == 'c')
-		ft_putchar_fd(va_arg(args, int), 1);
-	if (check_char == 's')
-		ft_putstr_fd(va_arg(args, char *), 1);
-}
+	char	*str;
 
+	if (check_char == 'c')
+	{
+		ft_putchar_fd(va_arg(args, int), 1);
+		*len = *len + 1;
+	}
+	if (check_char == 's')
+	{
+		str = va_arg(args, char *);
+		ft_putstr_fd(str, 1);
+	}
+}
 
 int	ft_printf(const char	*format, ...)
 {
@@ -34,70 +41,14 @@ int	ft_printf(const char	*format, ...)
 	{
 		if (format[i] == '%')
 		{
-			print_line(format[i + 1], args);
+			print_line(format[i + 1], args, &length);
 			i++;
 		}
 		else
 			ft_putchar_fd(format[i], 1);
 		i++;
+		length++;
 	}
-
+	printf("len = %d\n", length);
 	return (length);
 }
-
-
-// void mdr_tibo_kastou(const char *str, ...)
-// {
-// 	int i;
-// 	va_list args;
-
-// 	va_start(args, format);
-// 	i = -1;
-// 	while(++i)
-// 		if (str[i] == '%')
-// 			handle_print(str[i + 1], args);
-// 		else
-// 			ft_putchar_fd(str[i], 1);
-// }
-// void handle_print(char c, va_list args)
-// {
-// 	if (c == 'c')
-// 		write(1, va_arg(args, int), 1);
-// // }
-//return position du 1 caracte ok dans le string
-// int	ft_str_index_str(const char *where, const char *what)
-// {
-// 	int	i;
-// 	int	y;
-
-// 	i = 0;
-// 	if (!what[0] || !where[0])
-// 		return (0);
-// 	while (where[i])
-// 	{
-// 		y = 0;
-// 		while (where[i + y] == what[y])
-// 		{
-// 			if (what[y + 1] == '\0')
-// 			{
-// 				return (i);
-// 			}
-// 			y++;
-// 		}
-// 		i++;
-// 	}
-// 	return (0);
-// }
-// int	ft_str_index_chr(char *str, char c)
-// {
-// 	size_t	i;
-
-// 	i = 0;
-// 	while (i < ft_strlen(str) && str[i] != '\0')
-// 	{
-// 		if (str[i] == c)
-// 			return (i);
-// 		i++;
-// 	}
-// 	return (-1);
-// }
